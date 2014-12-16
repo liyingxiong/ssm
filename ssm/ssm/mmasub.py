764321dcdfd38b2001fca78be93d385ec95654b5
Li Yingxiong <Li Yingxiong@134.130.81.133>
2014-12-16 19:18:42 +0100

    MMA finished

---
+++ ssm/ssm/mmasub.py
@@ -0,0 +1,135 @@
+'''
+Created on Dec 1, 2014
+
+@author: Li Yingxiong
+'''
+import numpy as np
+from scipy.sparse import csr_matrix, spdiags
+from subsolv import subsolv
+
+def mmasub(m, n, iter, xval, xmin, xmax, xold1, xold2, \
+           f0val, df0dx, fval, dfdx, low, upp, a0, a, c, d):
+    '''
+    INPUT:
+    m     = The number of general constraints.
+    n     = The number of variables x_j.
+    iter  = Current iteration number ( =1 the first time mmasub is called).
+    xval  = Column vector with the current values of the variables x_j.
+    xmin  = Column vector with the lower bounds for the variables x_j.
+    xmax  = Column vector with the upper bounds for the variables x_j.
+    xold1 = xval, one iteration ago (provided that iter>1).
+    xold2 = xval, two iterations ago (provided that iter>2).
+    f0val = The value of the objective function f_0 at xval.
+    df0dx = Column vector with the derivatives of the objective function
+            f_0 with respect to the variables x_j, calculated at xval.
+    fval  = Column vector with the values of the constraint functions f_i,
+            calculated at xval.
+    dfdx  = (m x n)-matrix with the derivatives of the constraint functions
+            f_i with respect to the variables x_j, calculated at xval.
+            dfdx(i,j) = the derivative of f_i with respect to x_j.
+    low   = Column vector with the lower asymptotes from the previous
+            iteration (provided that iter>1).
+    upp   = Column vector with the upper asymptotes from the previous
+            iteration (provided that iter>1).
+    a0    = The constants a_0 in the term a_0*z.
+    a     = Column vector with the constants a_i in the terms a_i*z.
+    c     = Column vector with the constants c_i in the terms c_i*y_i.
+    d     = Column vector with the constants d_i in the terms 0.5*d_i*(y_i)^2.
+    
+    OUTPUT:
+    xmma  = Column vector with the optimal values of the variables x_j
+            in the current MMA subproblem.
+    ymma  = Column vector with the optimal values of the variables y_i
+            in the current MMA subproblem.
+    zmma  = Scalar with the optimal value of the variable z
+            in the current MMA subproblem.
+    lam   = Lagrange multipliers for the m general MMA constraints.
+    xsi   = Lagrange multipliers for the n constraints alfa_j - x_j <= 0.
+    eta   = Lagrange multipliers for the n constraints x_j - beta_j <= 0.
+     mu   = Lagrange multipliers for the m constraints -y_i <= 0.
+    zet   = Lagrange multiplier for the single constraint -z <= 0.
+     s    = Slack variables for the m general MMA constraints.
+    low   = Column vector with the lower asymptotes, calculated and used
+            in the current MMA subproblem.
+    upp   = Column vector with the upper asymptotes, calculated and used
+            in the current MMA subproblem.
+    '''
+    epsimin = 1e-7
+    raa0 = 0.00001
+    albefa = 0.1
+    asyinit =0.5 #0.2e-2;
+    asyincr = 1.2
+    asydecr = 0.7
+    eeen = np.ones(n,dtype=float)
+    eeem = np.ones(m,dtype=float)
+    zeron = np.zeros(n,dtype=float)
+    
+    # Calculation of the asymptotes low and upp :
+    if iter < 2.5:
+        low = xval - asyinit*(xmax-xmin)
+        upp = xval + asyinit*(xmax-xmin)
+    else:
+        zzz = (xval-xold1)*(xold1-xold2)
+        factor = eeen
+        factor[zzz > 0] = asyincr
+        factor[zzz < 0] = asydecr
+        low = xval - factor*(xold1 - low)
+        upp = xval + factor*(upp - xold1)
+        lowmin = xval - 10*(xmax-xmin)
+        lowmax = xval - 0.01*(xmax-xmin)
+        uppmin = xval + 0.01*(xmax-xmin)
+        uppmax = xval + 10*(xmax-xmin)
+        low = np.maximum(low,lowmin)
+        low = np.minimum(low,lowmax)
+        upp = np.minimum(upp,uppmax)
+        upp = np.maximum(upp,uppmin)
+      
+    # Calculation of the bounds alfa and beta :
+    zzz = low + albefa*(xval-low)
+    alfa = np.maximum(zzz,xmin)
+    zzz = upp - albefa*(upp-xval)
+    beta = np.minimum(zzz,xmax)
+    
+    # Calculations of p0, q0, P, Q and b.
+    xmami = xmax-xmin
+    xmamieps = 0.00001*eeen
+    xmami = np.maximum(xmami,xmamieps)
+    xmamiinv = eeen/xmami
+    ux1 = upp-xval
+    ux2 = ux1*ux1
+    xl1 = xval-low
+    xl2 = xl1*xl1
+    uxinv = eeen/ux1
+    xlinv = eeen/xl1
+    #
+#     p0 = zeron
+#     q0 = zeron
+    p0 = np.maximum(df0dx, 0)
+    q0 = np.maximum(-df0dx, 0)
+    #p0(find(df0dx > 0)) = df0dx(find(df0dx > 0));
+    #q0(find(df0dx < 0)) = -df0dx(find(df0dx < 0));
+    pq0 = 0.001*(p0 + q0) + raa0*xmamiinv
+    p0 = p0 + pq0
+    q0 = q0 + pq0
+    p0 = p0*ux2
+    q0 = q0*xl2
+
+    P = np.maximum(dfdx, 0)
+    Q = np.maximum(-dfdx, 0)
+    #P(find(dfdx > 0)) = dfdx(find(dfdx > 0));
+    #Q(find(dfdx < 0)) = -dfdx(find(dfdx < 0));
+    PQ = 0.001*(P + Q) + raa0*eeem[:, None]*xmamiinv[None, :]
+    P = P + PQ
+    Q = Q + PQ
+    P = csr_matrix(P)*spdiags(ux2,0,n,n)
+    Q = csr_matrix(Q)*spdiags(xl2,0,n,n)
+    b = P.dot(uxinv) + Q.dot(xlinv) - fval
+    
+    #
+    ### Solving the subproblem by a primal-dual Newton method
+    xmma,ymma,zmma,lam,xsi,eta,mu,zet,s = \
+    subsolv(m,n,epsimin,low,upp,alfa,beta,p0,q0,P,Q,a0,a,b,c,d)
+    
+    return xmma,ymma,zmma,lam,xsi,eta,mu,zet,s,low,upp
+      
+    
---
+++ ssm/ssm/subsolv.py
@@ -0,0 +1,213 @@
+'''
+Created on Dec 2, 2014
+
+@author: Li Yingxiong
+'''
+import numpy as np
+from scipy.sparse import spdiags, csr_matrix
+
+
+def subsolv(m,n,epsimin,low,upp,alfa,beta,p0,q0,P,Q,a0,a,b,c,d):
+    '''
+    INPUT:
+    m : integer
+    n : integer
+    epsimin : float 1e-7
+    low : Column vector 
+    upp : Column vector
+    alfa : Column vector
+    beta : Column vector
+    p0 : Column vector length n
+    q0 : Column vector length n
+    P : m x n sparse matrix
+    Q : m x n sparse matrix
+    a0 : constants
+    a,b,c,d : Column vector
+    '''
+      
+    een = np.ones(n,dtype=float)
+    eem = np.ones(m,dtype=float)
+    epsi = 1
+    epsvecn = epsi*een
+    epsvecm = epsi*eem
+    x = 0.5*(alfa+beta)
+    y = eem
+    z = 1
+    lam = eem
+    xsi = een/(x-alfa)
+    xsi = np.amax((xsi,een), axis=0)
+    eta = een/(beta-x)
+    eta = np.amax((eta,een), axis=0)
+    mu  = np.amax((eem,0.5*c), axis=0)
+    zet = 1
+    s = eem
+    itera = 0
+    
+    while epsi > epsimin:
+        epsvecn = epsi*een
+        epsvecm = epsi*eem
+        ux1 = upp-x
+        xl1 = x-low
+        ux2 = ux1*ux1
+        xl2 = xl1*xl1
+        uxinv1 = een/ux1
+        xlinv1 = een/xl1
+        
+        
+        plam = p0 + P.T.dot(lam) 
+        qlam = q0 + Q.T.dot(lam) 
+        gvec = P.dot(uxinv1) + Q.dot(xlinv1)
+        dpsidx = plam/ux2 - qlam/xl2 
+    
+        rex = dpsidx - xsi + eta
+        rey = c + d*y - mu - lam
+        rez = a0 - zet - np.dot(a, lam)
+        relam = gvec - a*z - y + s - b
+        rexsi = xsi*(x-alfa) - epsvecn
+        reeta = eta*(beta-x) - epsvecn
+        remu = mu*y - epsvecm
+        rezet = zet*z - epsi
+        res = lam*s - epsvecm
+    
+        residu1 = np.hstack((rex, rey, rez))
+        residu2 = np.hstack((relam, rexsi, reeta, remu, rezet, res))
+        residu = np.hstack((residu1, residu2))
+        residunorm = np.sqrt(np.dot(residu,residu))
+        residumax = max(abs(residu))
+    
+        ittt = 0
+        while residumax > 0.9*epsi and ittt < 100:
+            ittt=ittt + 1
+            itera=itera + 1
+        
+            ux1 = upp-x
+            xl1 = x-low
+            ux2 = ux1*ux1
+            xl2 = xl1*xl1
+            ux3 = ux1*ux2
+            xl3 = xl1*xl2
+            uxinv1 = een/ux1
+            xlinv1 = een/xl1
+            uxinv2 = een/ux2
+            xlinv2 = een/xl2
+            plam = p0 + P.T.dot(lam) 
+            qlam = q0 + Q.T.dot(lam) 
+            gvec = P.dot(uxinv1) + Q.dot(xlinv1)
+            GG = P*spdiags(uxinv2,0,n,n) - Q*spdiags(xlinv2,0,n,n)
+            dpsidx = plam/ux2 - qlam/xl2 
+            delx = dpsidx - epsvecn/(x-alfa) + epsvecn/(beta-x)
+            dely = c + d*y - lam - epsvecm/y
+            delz = a0 - np.dot(a, lam) - epsi/z
+            dellam = gvec - a*z - y - b + epsvecm/lam
+            diagx = plam/ux3 + qlam/xl3
+            diagx = 2*diagx + xsi/(x-alfa) + eta/(beta-x)
+            diagxinv = een/diagx
+            diagy = d + mu/y
+            diagyinv = eem/diagy
+            diaglam = s/lam
+            diaglamyi = diaglam+diagyinv
+        
+            if m < n:
+                blam = dellam + dely/diagy - GG.dot(delx/diagx)
+                bb = np.hstack((blam, delz))
+                Alam = spdiags(diaglamyi,0,m,m) + GG*spdiags(diagxinv,0,n,n)*GG.T
+                AA = np.vstack(( np.hstack((Alam.todense(), a[:, None])), \
+                                 np.hstack((a, -zet/z)) ))
+                solut = np.linalg.solve(AA, bb)
+                dlam = solut[0:m]
+                dz = solut[m]
+                dx = -delx/diagx - GG.T.dot(dlam)/diagx
+            else:
+                diaglamyiinv = eem/diaglamyi
+                dellamyi = dellam + dely/diagy
+                Axx = spdiags(diagx,0,n,n) + GG.T*spdiags(diaglamyiinv,0,m,m)*GG
+                azz = zet/z + np.dot(a, a/diaglamyi)
+                axz = -GG.T.dot(a/diaglamyi)
+                bx = delx + GG.T.dot(dellamyi/diaglamyi)
+                bz  = delz - np.dot(a, dellamyi/diaglamyi)
+                AA = np.vstack(( np.hstack((Axx.todense(), axz[:, None])), \
+                                 np.hstack((axz, azz)) ))
+                bb = np.hstack((-bx, -bz))
+                solut = np.linalg.solve(AA, bb)
+                dx  = solut[0:n]
+                dz = solut[n]
+                dlam = GG.dot(dx)/diaglamyi - dz*(a/diaglamyi) + dellamyi/diaglamyi
+        
+            dy = -dely/diagy + dlam/diagy
+            dxsi = -xsi + epsvecn/(x-alfa) - (xsi*dx)/(x-alfa)
+            deta = -eta + epsvecn/(beta-x) + (eta*dx)/(beta-x)
+            dmu  = -mu + epsvecm/y - (mu*dy)/y
+            dzet = -zet + epsi/z - zet*dz/z
+            ds   = -s + epsvecm/lam - (s*dlam)/lam
+            xx  = np.hstack((y, z, lam, xsi, eta, mu, zet, s))
+            dxx = np.hstack((dy, dz, dlam, dxsi, deta, dmu, dzet, ds))
+            
+            stepxx = -1.01*dxx/xx
+            stmxx  = max(stepxx)
+            stepalfa = -1.01*dx/(x-alfa)
+            stmalfa = max(stepalfa)
+            stepbeta = 1.01*dx/(beta-x)
+            stmbeta = max(stepbeta)
+            stmalbe  = max(stmalfa,stmbeta)
+            stmalbexx = max(stmalbe,stmxx)
+            stminv = max(stmalbexx,1)
+            steg = 1/stminv
+        
+            xold   =   x
+            yold   =   y
+            zold   =   z
+            lamold =  lam
+            xsiold =  xsi
+            etaold =  eta
+            muold  =  mu
+            zetold =  zet
+            sold   =   s
+        
+            itto = 0
+            resinew = 2*residunorm
+            while resinew > residunorm and itto < 50:
+                itto = itto+1
+            
+                x   =   xold + steg*dx
+                y   =   yold + steg*dy
+                z   =   zold + steg*dz
+                lam = lamold + steg*dlam
+                xsi = xsiold + steg*dxsi
+                eta = etaold + steg*deta
+                mu  = muold  + steg*dmu
+                zet = zetold + steg*dzet
+                s   =   sold + steg*ds
+                ux1 = upp-x
+                xl1 = x-low
+                ux2 = ux1*ux1
+                xl2 = xl1*xl1
+                uxinv1 = een/ux1
+                xlinv1 = een/xl1
+                plam = p0 + P.T.dot(lam) 
+                qlam = q0 + Q.T.dot(lam) 
+                gvec = P.dot(uxinv1) + Q.dot(xlinv1)
+                dpsidx = plam/ux2 - qlam/xl2 
+            
+                rex = dpsidx - xsi + eta
+                rey = c + d*y - mu - lam
+                rez = a0 - zet - np.dot(a, lam)
+                relam = gvec - a*z - y + s - b
+                rexsi = xsi*(x-alfa) - epsvecn
+                reeta = eta*(beta-x) - epsvecn
+                remu = mu*y - epsvecm
+                rezet = zet*z - epsi
+                res = lam*s - epsvecm
+            
+                residu1 = np.hstack((rex, rey, rez))
+                residu2 = np.hstack((relam, rexsi, reeta, remu, rezet, res))
+                residu = np.hstack((residu1, residu2))
+                resinew = np.sqrt(np.dot(residu, residu))
+                steg = steg/2
+
+            residunorm=resinew
+            residumax = max(abs(residu))
+            steg = 2*steg
+        
+        epsi = 0.1*epsi
+    
+    return x, y, z, lam, xsi, eta, mu, zet, s
